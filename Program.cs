470cde21d63ac4abd37f45918add3b7e2024d7bb
Opilane TTHK <opilane@tthk.ee>
2022-03-02 14:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+    public class Nelinurkk
+    {
+        double korgus, laius;
+        Tip tip;
+
+        public Nelinurkk() { }
+        public Nelinurkk(double Korgus, double Laius, Tip tyyp)
+        {
+            korgus = Korgus;
+            laius = Laius;
+            this.tip = tip;
+        }
+        public double Korgus
+        {
+            set { korgus = value; }
+            get { return korgus; }
+        }
+        public double Korgus
+        {
+            set { korgus = value; }
+            get { return korgus; }
+        }
+
+    }
+
+}
